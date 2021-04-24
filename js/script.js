1bdd19aae78a3e51217534e8c357e7b6e968f3d6
sushi-chan <nilbuyukkinaci@gmail.com>
2021-04-24 19:23:32 +0300

    Ortaokullara odaklı geometrik şekiller konusunda çalışma sitesi

---
+++ js/script.js
@@ -0,0 +1,279 @@
+function KareKontrol()
+{
+	let kutu1 = document.querySelector("#kare1");
+	var cevap1 = document.getElementById("kare1").value;
+	if(cevap1 == "4")
+		kutu1.style.backgroundColor = "#00FF00";
+	else
+		kutu1.style.backgroundColor = "#FF0000";
+
+	let kutu2 = document.querySelector("#kare2");
+	var cevap2 = document.getElementById("kare2").value;
+	if(cevap1 == "4")
+		kutu2.style.backgroundColor = "#00FF00";
+	else
+		kutu2.style.backgroundColor = "#FF0000";
+
+	let kutu3 = document.querySelector("#kare3");
+	var cevap3 = document.getElementById("kare3").value;
+	if(cevap3 == "360")
+		kutu3.style.backgroundColor = "#00FF00";
+	else
+		kutu3.style.backgroundColor = "#FF0000";
+
+	let kutu4 = document.querySelector("#kare4");
+	var cevap4 = document.getElementById("kare4").value;
+	if(cevap4 == "360")
+		kutu4.style.backgroundColor = "#00FF00";
+	else
+		kutu4.style.backgroundColor = "#FF0000";
+
+	let kutu5 = document.querySelector("#kare5");
+	var cevap5 = document.getElementById("kare5").value;
+	if(cevap5 == "90")
+		kutu5.style.backgroundColor = "#00FF00";
+	else
+		kutu5.style.backgroundColor = "#FF0000";
+}
+
+function KareYenile()
+{
+	let kutu1 = document.querySelector("#kare1");
+	kutu1.style.backgroundColor = "#FFFFFF";
+
+	let kutu2 = document.querySelector("#kare2");
+	kutu2.style.backgroundColor = "#FFFFFF";
+
+	let kutu3 = document.querySelector("#kare3");
+	kutu3.style.backgroundColor = "#FFFFFF";
+
+	let kutu4 = document.querySelector("#kare4");
+	kutu4.style.backgroundColor = "#FFFFFF";
+
+	let kutu5 = document.querySelector("#kare5");
+	kutu5.style.backgroundColor = "#FFFFFF";
+}
+
+function UcgenKontrol()
+{
+	let kutu1 = document.querySelector("#u1");
+	var cevap1 = document.getElementById("u1").value;
+	if(cevap1 == "3")
+		kutu1.style.backgroundColor = "#00FF00";
+	else
+		kutu1.style.backgroundColor = "#FF0000";
+
+	let kutu2 = document.querySelector("#u2");
+	var cevap2 = document.getElementById("u2").value;
+	if(cevap1 == "3")
+		kutu2.style.backgroundColor = "#00FF00";
+	else
+		kutu2.style.backgroundColor = "#FF0000";
+
+	let kutu3 = document.querySelector("#u3");
+	var cevap3 = document.getElementById("u3").value;
+	if(cevap3 == "180")
+		kutu3.style.backgroundColor = "#00FF00";
+	else
+		kutu3.style.backgroundColor = "#FF0000";
+
+	let kutu4 = document.querySelector("#u4");
+	var cevap4 = document.getElementById("u4").value;
+	if(cevap4 == "360")
+		kutu4.style.backgroundColor = "#00FF00";
+	else
+		kutu4.style.backgroundColor = "#FF0000";
+
+	let kutu5 = document.querySelector("#u5");
+	var cevap5 = document.getElementById("u5").value;
+	if(cevap5 == "60")
+		kutu5.style.backgroundColor = "#00FF00";
+	else
+		kutu5.style.backgroundColor = "#FF0000";
+}
+
+function UcgenYenile()
+{
+	let kutu1 = document.querySelector("#u1");
+	kutu1.style.backgroundColor = "#FFFFFF";
+
+	let kutu2 = document.querySelector("#u2");
+	kutu2.style.backgroundColor = "#FFFFFF";
+
+	let kutu3 = document.querySelector("#u3");
+	kutu3.style.backgroundColor = "#FFFFFF";
+
+	let kutu4 = document.querySelector("#u4");
+	kutu4.style.backgroundColor = "#FFFFFF";
+
+	let kutu5 = document.querySelector("#u5");
+	kutu5.style.backgroundColor = "#FFFFFF";
+}
+
+function DikdortgenKontrol()
+{
+	let kutu1 = document.querySelector("#d1");
+	var cevap1 = document.getElementById("d1").value;
+	if(cevap1 == "4")
+		kutu1.style.backgroundColor = "#00FF00";
+	else
+		kutu1.style.backgroundColor = "#FF0000";
+
+	let kutu2 = document.querySelector("#d2");
+	var cevap2 = document.getElementById("d2").value;
+	if(cevap1 == "4")
+		kutu2.style.backgroundColor = "#00FF00";
+	else
+		kutu2.style.backgroundColor = "#FF0000";
+
+	let kutu3 = document.querySelector("#d3");
+	var cevap3 = document.getElementById("d3").value;
+	if(cevap3 == "360")
+		kutu3.style.backgroundColor = "#00FF00";
+	else
+		kutu3.style.backgroundColor = "#FF0000";
+
+	let kutu4 = document.querySelector("#d4");
+	var cevap4 = document.getElementById("d4").value;
+	if(cevap4 == "360")
+		kutu4.style.backgroundColor = "#00FF00";
+	else
+		kutu4.style.backgroundColor = "#FF0000";
+
+	let kutu5 = document.querySelector("#d5");
+	var cevap5 = document.getElementById("d5").value;
+	if(cevap5 == "90")
+		kutu5.style.backgroundColor = "#00FF00";
+	else
+		kutu5.style.backgroundColor = "#FF0000";
+}
+
+function DikdortgenYenile()
+{
+	let kutu1 = document.querySelector("#d1");
+	kutu1.style.backgroundColor = "#FFFFFF";
+
+	let kutu2 = document.querySelector("#d2");
+	kutu2.style.backgroundColor = "#FFFFFF";
+
+	let kutu3 = document.querySelector("#d3");
+	kutu3.style.backgroundColor = "#FFFFFF";
+
+	let kutu4 = document.querySelector("#d4");
+	kutu4.style.backgroundColor = "#FFFFFF";
+
+	let kutu5 = document.querySelector("#d5");
+	kutu5.style.backgroundColor = "#FFFFFF";
+}
+
+function BesgenKontrol()
+{
+	let kutu1 = document.querySelector("#b1");
+	var cevap1 = document.getElementById("b1").value;
+	if(cevap1 == "5")
+		kutu1.style.backgroundColor = "#00FF00";
+	else
+		kutu1.style.backgroundColor = "#FF0000";
+
+	let kutu2 = document.querySelector("#b2");
+	var cevap2 = document.getElementById("b2").value;
+	if(cevap1 == "5")
+		kutu2.style.backgroundColor = "#00FF00";
+	else
+		kutu2.style.backgroundColor = "#FF0000";
+
+	let kutu3 = document.querySelector("#b3");
+	var cevap3 = document.getElementById("b3").value;
+	if(cevap3 == "540")
+		kutu3.style.backgroundColor = "#00FF00";
+	else
+		kutu3.style.backgroundColor = "#FF0000";
+
+	let kutu4 = document.querySelector("#b4");
+	var cevap4 = document.getElementById("b4").value;
+	if(cevap4 == "360")
+		kutu4.style.backgroundColor = "#00FF00";
+	else
+		kutu4.style.backgroundColor = "#FF0000";
+
+	let kutu5 = document.querySelector("#b5");
+	var cevap5 = document.getElementById("b5").value;
+	if(cevap5 == "108")
+		kutu5.style.backgroundColor = "#00FF00";
+	else
+		kutu5.style.backgroundColor = "#FF0000";
+}
+
+function BesgenYenile()
+{
+	let kutu1 = document.querySelector("#b1");
+	kutu1.style.backgroundColor = "#FFFFFF";
+
+	let kutu2 = document.querySelector("#b2");
+	kutu2.style.backgroundColor = "#FFFFFF";
+
+	let kutu3 = document.querySelector("#b3");
+	kutu3.style.backgroundColor = "#FFFFFF";
+
+	let kutu4 = document.querySelector("#b4");
+	kutu4.style.backgroundColor = "#FFFFFF";
+
+	let kutu5 = document.querySelector("#b5");
+	kutu5.style.backgroundColor = "#FFFFFF";
+}
+
+function AltigenKontrol()
+{
+	let kutu1 = document.querySelector("#a1");
+	var cevap1 = document.getElementById("a1").value;
+	if(cevap1 == "6")
+		kutu1.style.backgroundColor = "#00FF00";
+	else
+		kutu1.style.backgroundColor = "#FF0000";
+
+	let kutu2 = document.querySelector("#a2");
+	var cevap2 = document.getElementById("a2").value;
+	if(cevap1 == "6")
+		kutu2.style.backgroundColor = "#00FF00";
+	else
+		kutu2.style.backgroundColor = "#FF0000";
+
+	let kutu3 = document.querySelector("#a3");
+	var cevap3 = document.getElementById("a3").value;
+	if(cevap3 == "720")
+		kutu3.style.backgroundColor = "#00FF00";
+	else
+		kutu3.style.backgroundColor = "#FF0000";
+
+	let kutu4 = document.querySelector("#a4");
+	var cevap4 = document.getElementById("a4").value;
+	if(cevap4 == "360")
+		kutu4.style.backgroundColor = "#00FF00";
+	else
+		kutu4.style.backgroundColor = "#FF0000";
+
+	let kutu5 = document.querySelector("#a5");
+	var cevap5 = document.getElementById("a5").value;
+	if(cevap5 == "120")
+		kutu5.style.backgroundColor = "#00FF00";
+	else
+		kutu5.style.backgroundColor = "#FF0000";
+}
+
+function AltigenYenile()
+{
+	let kutu1 = document.querySelector("#a1");
+	kutu1.style.backgroundColor = "#FFFFFF";
+
+	let kutu2 = document.querySelector("#a2");
+	kutu2.style.backgroundColor = "#FFFFFF";
+
+	let kutu3 = document.querySelector("#a3");
+	kutu3.style.backgroundColor = "#FFFFFF";
+
+	let kutu4 = document.querySelector("#a4");
+	kutu4.style.backgroundColor = "#FFFFFF";
+
+	let kutu5 = document.querySelector("#a5");
+	kutu5.style.backgroundColor = "#FFFFFF";
+}
